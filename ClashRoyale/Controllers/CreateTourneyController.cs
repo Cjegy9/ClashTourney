6c03c6fbac250728f0b2d629871a7aed06a0de55
Colin Eyster <cjegy9@mst.edu>
2016-03-09 15:24:18 -0600

    Separate controllers for the major pages.

---
+++ ClashRoyale/Controllers/CreateTourneyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClashRoyale.Controllers
+{
+    public class CreateTourneyController : Controller
+    {
+        // GET: CreateTourney
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ClashRoyale/Controllers/HomeController.cs
+++ ClashRoyale/Controllers/HomeController.cs
@@ -13,16 +13,16 @@ namespace ClashRoyale.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult CreateTourney()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult JoinTourney()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
